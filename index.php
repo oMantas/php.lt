e7d86a7c6ef02743d27c806265bd5c8932d04f87
Mantas <mantas.aaaa@gmail.com>
2019-01-21 11:55:49 +0200

    asda

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
+$random = rand(1,10);
+$random2 = rand(1,5);
+$random3 = rand(2,10);
 ?>
 <!DOCTYPE html>
     <html lang="en">
@@ -7,10 +9,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Komentarai</title>
+        <title>PHP lydes ir <?php print date('Y.m.d', strtotime("$random year"));?></title>
     </head>
     <body>
-        <div><!-- <?php print "belekas"?> --></div>
-        <div>Kazkas</div>
+       <h1>Mantas - galbut turesiu <?php print "$random2";?> vaiku.</h1>
+       <p>D.Trumpas nebebus prezidentu <?php print date('Y.m.d', strtotime("$random3 year"));?></p>
     </body>
 </html>
