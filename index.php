816395519b141ccd0af6e4c45f5b7c141716c117
Mantas <mantas.aaaa@gmail.com>
2019-01-21 13:29:17 +0200

    bomba

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-$rand = rand(1,6);
+$seconds = date('s');
+$bomb_size = 100 + date('s');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,9 +9,16 @@ $rand = rand(1,6);
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bomb.css">
+        <style>
+            img {
+                height: <?php print $bomb_size?>px;
+                width: <?php print $bomb_size?>px;
+            }
+        </style>
     </head>
-    <body id="dice<?php print $rand;?>">
-        
+    <body>
+        <img src='../img/bomb.png'>
+        <p><?php print $seconds;?></p>
     </body>
 </html>
