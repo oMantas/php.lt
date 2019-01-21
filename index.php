caab796e93c84b94759cae847fe372adab3668a3
Mantas <mantas.aaaa@gmail.com>
2019-01-21 11:02:40 +0200

    pataisyti tarpai strtotime

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ date_default_timezone_set("Europe/Vilnius");
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>PHP lydes ir <?php print date('Y.m.d',strtotime('+1 day'));?></title>
+        <title>PHP lydes ir <?php print date('Y.m.d', strtotime('+1 day'));?></title>
     </head>
     <body>
-        <h1>Mantas - PHP su manim buvo ir <?php print date('h.i.s',strtotime('-1 hour'));?> valanda.</h1>
-        <p><?php print date('Y',strtotime('+1 year'))?> ne uz kalnu.</p>
+        <h1>Mantas - PHP su manim buvo ir <?php print date('h.i.s', strtotime('-1 hour'));?> valanda.</h1>
+        <p><?php print date('Y', strtotime('+1 year'))?> ne uz kalnu.</p>
     </body>
 </html>
