754396f953c53d4a360fcbb4d2be490a4a77e885
Mantas <mantas.aaaa@gmail.com>
2019-01-21 10:58:58 +0200

    2uzd

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 date_default_timezone_set("Europe/Vilnius");
-$kelintadienis = date('l');
-$metai = date('Y');
-$menuo = date('m');
-$day = date('d');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,10 +7,10 @@ $day = date('d');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>As, <?php print $kelintadienis;?> ir PHP</title>
+        <title>PHP lydes ir <?php print date('Y.m.d',strtotime('+1 day'));?></title>
     </head>
     <body>
-        <h1>Mantas - HTML ir PHP asas jau nuo <?php print $metai;?> metu.</h1>
-        <p>Viskas prasidejo <?php print $menuo;?> menesio, <?php print $day;?> diena.</p>
+        <h1>Mantas - PHP su manim buvo ir <?php print date('h.i.s',strtotime('-1 hour'));?> valanda.</h1>
+        <p><?php print date('Y',strtotime('+1 year'))?> ne uz kalnu.</p>
     </body>
 </html>
