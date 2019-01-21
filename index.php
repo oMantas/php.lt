361c1af9131828fe39546b2eb63cdd5cda1b000f
Mantas <mantas.aaaa@gmail.com>
2019-01-21 13:54:59 +0200

    darbas baigtas

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-$seconds = date('s');
-$bomb_size = 100 + date('s');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,16 +8,8 @@ $bomb_size = 100 + date('s');
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-        <link rel="stylesheet" href="css/bomb.css">
-        <style>
-            img {
-                height: <?php print $bomb_size; ?>px;
-                width: <?php print $bomb_size; ?>px;
-            }
-        </style>
     </head>
     <body>
-        <img src='../img/bomb.png'>
-        <p><?php print $seconds; ?></p>
+        
     </body>
 </html>
