ae85d6353bc037709570ec32dcb67d0d9e05e9ad
Mantas <mantas.aaaa@gmail.com>
2019-01-23 17:17:15 +0200

    pataisyta

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 $days = 365;
-$cizos_mon_thu = rand(3,4);
-$cizos_fri = rand(10,20);
-$cizos_sat_sun = rand(1,3);
 $cig_cost = 3.50 / 20;
 $viso_vnt = 0;
 $mon_thu_smoked_cig_per_year = 0;
 
 for($i = 0; $i < $days; $i++) {
     $weekday = date('N', strtotime("+$i day"));
+    $cizos_mon_thu = rand(3,4);
+    $cizos_fri = rand(10,20);
+    $cizos_sat_sun = rand(1,3);
     if($weekday < 5) {
         $viso_vnt += $cizos_mon_thu;
         $mon_thu_smoked_cig_per_year += $cizos_mon_thu;
