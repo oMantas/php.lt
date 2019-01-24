77f2be0144c3a0c3e623dd327e43464038356b72
Mantas <mantas.aaaa@gmail.com>
2019-01-24 18:38:17 +0200

    kazkas

--- index.php
+++ index.php
@@ -1,21 +1,64 @@
 <?php
-$belekas = [
-    'zmogus1' => [
-        'vardas_pavarde' => 'Petras Pirdzius',
-        'ka_turi' => 'telefona',
-        'koki' => 'prapista',
+$produktai = [
+    'pieno_produktai' => [
+        'grietine' => [
+            'pavadinimas' => 'DVARO',
+            'kiekis' => 1,
+            'mase' => 'nesimato',
+            'riebumas' => 'nesimato',
+
+        ],
+        'jogurtas' => [
+            'pavadinimas' => 'KAZKAS SU KATEM',
+            'kiekis' => 2,
+            'mase' => 'nesimato',
+            'riebumas' => 'nesimato',
+        ],
+        'pienas' => [
+            'pavadinimas' => 'DVARO',
+            'kiekis' => 1,
+            'mase' => 'speju 1litras',
+            'riebumas' => 'nesimato',
+        ],
+        'kefyras' => [
+            'pavadinimas' => 'VISIEM PX',
+            'kiekis' => 'siuo metu nera',
+            'mase' => 'turbut irgi nesimatytu',
+            'riebumas' => 'visiskai px',
+        ]
     ],
-    'zmogus2' => [
-        'vardas_pavarde' => 'Tomas Ablomas',
-        'ka_turi' => 'buljona',
-        'koki' => 'naudojama',
+    'maistas' => [
+        'kiausiniai' => [
+            'pavadinimas' => 'KAZKOKS PAUKSTYNAS ir t.t',
+            'kiekis' => 3,
+            'mase' => 'speju M',
+            'kalorijos' => 'pasigooglink',
+        ],
+        'zuvis' => [
+            'pavadinimas' => 'ISIVAIZDUOJAMA',
+            'kiekis' => 'siuo metu nera',
+            'mase' => 'priklauso nuo pasaro',
+            'kalorijos' => 'google allmighty',
+        ]
     ],
-    'zmogus3' => [
-        'vardas_pavarde' => 'Ana Shitkova',
-        'ka_turi' => 'rulona',
-        'koki' => 'pasibaigusi',
-    ],     
+    'padazai' => [
+        'keciupas' => [
+            'pavadinimas' => 'CIUMAK',
+            'kiekis' => 2,
+            'mase' => 'speju 750ml',
+            'kalorijos' => 'speju, kad visiem px',
+        ]
+    ],
+    'alkoholis' => [
+        'stiprieji' => [
+            'pavadinimas' => 'STUMBRAS',
+            'rusis' => 'degtine',
+            'kiekis' => 2,
+            'mase' => '1 litras',
+            'kalorijos' => 'speju, kad visiem visiskai tikrai px',
+        ]
+    ],
 ];
 
-var_dump($belekas);
+var_dump($produktai);
 ?>
