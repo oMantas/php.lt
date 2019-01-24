776e4a64d1ff8cc59c784bbd909bd7d106142a9f
Mantas <mantas.aaaa@gmail.com>
2019-01-24 13:03:21 +0200

    adasd

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $sudo_receptas = [
-    'Kiausiniai' => [2, 'dideli'],
-    'Baklazanas' => [1, 'mazas'],
-    'Grietines indelis' => [1, 'didelis'],
+    'Kiausiniai' => ['dideli' => 2],
+    'Baklazanas' => ['mazas' => 1],
+    'Grietines indelis' => ['didelis' => 1],
 ];
 
 var_dump($sudo_receptas);
