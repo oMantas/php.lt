5961815b58d890ffadbe7b47295ca28f2b186b93
Mantas <mantas.aaaa@gmail.com>
2019-01-24 12:55:41 +0200

    asd

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
-/* $maisto_produktai = ['Obuolys', 'Apelsinas', 'Cukinija'];
-print var_dump($maisto_produktai); */
 $sudo_receptas = [
-    "Dideli kiausiniai" => 2,
-    "Mazas Baklazanas" => 1,
-    "Didelis grietines indelis" => 1,
+    'Dideli kiausiniai' => '2vnt',
+    'Mazasa baklazanas' => '1vnt',
+    'Didelis grietines indelis' => '1vnt',
 ];
 
 print var_dump($sudo_receptas);
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-</html>
