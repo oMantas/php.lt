7c81f823740930f44edf4a7afaa3b5a3938da750
Mantas <mantas.aaaa@gmail.com>
2019-01-28 11:34:35 +0200

    1uzd

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
-
+$mano_atmintis = ['Penktadienis', 'miegojau', 'atsikeliau', 'valgiau', 'geriau', 'viskas blanku', 'miegojau'];
 ?>
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>WTF?!</h1>
+        <h2>Mano atmintis</h2>
+        <p>
+            <ul>
+                <?php foreach ($mano_atmintis as $value): ?>
+                    <li>
+                        <?php print $value; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </p>
+    </body>
 </html>
