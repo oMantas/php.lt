00b9eac52d4e734f81ca4660d4182c6f09b743d5
Mantas <mantas.aaaa@gmail.com>
2019-01-28 19:47:17 +0200

    namu darbai

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-$mano_atmintis = [
-    'Penktadienis', 'miegojau', 'atsikeliau',
-    'valgiau', 'geriau', 'viskas blanku', 'miegojau'
+$dishes = [
+    [
+        'name' => 'Nut salad',
+        'price' => 3.44,
+        'ingredients' => ['Nuts', 'Yogurt', 'Fruits'],
+        'photo' => ['nutSalad.jpg'],
+    ],
+    [
+        'name' => 'Chujnia',
+        'price' => 4.77,
+        'ingredients' => ['Rice', 'Soya Sauce', 'Sausage'],
+        'photo' => ['chujnia.jpg'],
+    ],
 ];
-$array_length = count($mano_atmintis);
-$rand = rand(0, $array_length - 1);
 
-$zmonos_atmintis = [
-    'Penktadienis', 'miegojau', 'atsikeliau',
-    'valgiau', 'geriau', 'viska atsimena'
-];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,27 +22,46 @@ $zmonos_atmintis = [
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <link rel="stylesheet" href="css/dish.css">
     </head>
     <body>
-        <h1>WTF?!</h1>
-        <h2>Mano atmintis</h2>
-        <h3>
-            <?php print $mano_atmintis[$rand]; ?>
-        </h3>
-        <ul>
-            <?php foreach ($mano_atmintis as $value): ?>
-                <li>
-                    <?php print $value; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-        <h2>Zmonos atmintis</h2>
-        <ul>
-            <?php foreach ($zmonos_atmintis as $value): ?>
-                <li>
-                    <?php print $value; ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+        <table>
+            <tr>
+                <td><span>Dishes</span></td>
+                <?php foreach ($dishes as $dish): ?>
+                    <th>
+                        <span class='tblHead'>
+                            <?php print $dish['name']; ?>
+                        </span>
+                    </th>
+                <?php endforeach; ?>
+            </tr>
+            <tr>
+                <td>Price (eur)</td>
+                <?php foreach ($dishes as $dish): ?>
+                    <td>
+                        <span><?php print $dish['price']; ?></span>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+            <tr>
+                <td>Ingredients</td>
+                <?php foreach ($dishes as $dish): ?>
+                    <td>
+                        <?php foreach ($dish['ingredients'] as $value): ?>
+                            <?php print "$value <br>"; ?>
+                        <?php endforeach; ?>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+            <tr id='photoRow'>
+                <td>Photo</td>
+                <?php foreach ($dishes as $dish): ?>
+                        <?php foreach ($dish['photo'] as $value): ?>
+                            <td class='bgImg' style='background-image: url(/img/<?php print $value; ?>)'></td>
+                        <?php endforeach; ?>
+                <?php endforeach; ?>
+            </tr>
+        </table>
     </body>
 </html>
