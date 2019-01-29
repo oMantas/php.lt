bc1dede00abbc94221ab1e46459bcdb693dcc0fc
Mantas <mantas.aaaa@gmail.com>
2019-01-29 16:40:27 +0200

    uzd 7

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
-function add($x, $y){
-    $suma = $x + $y;
-     return 'Suma:'. $suma;
-};
+$name = ['dildo', 'ilgas dildo', 'azijietiskas dildo', 'plonas dildo', 'storas dildo'];
+$daiktu_kiekis = rand(1, 10);
+
+for ($i = 0; $i < $daiktu_kiekis; $i++) {
+    $rand_daiktas = $name[rand(0, count($name) - 1)];
+    $size = rand(5, 33);
+    $is_dark = rand(0, 1);
+
+    if ($is_dark) {
+        $spalva = 'tamsus';
+    } else {
+        $spalva = 'sviesus';
+    };
+
+    $rankinukas[] = [
+        'name' => $rand_daiktas,
+        'size' => $size,
+        'color' => $spalva,
+        'info' => "$rand_daiktas uzima $size cm3. Daiktas: $spalva."
+
+    ];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,8 +31,10 @@ function add($x, $y){
         <title>Document</title>
     </head>
     <body>
-        <h1>
-            <?php print add(2, 2); ?>
-        </h1>
+        <?php foreach ($rankinukas as $daiktas): ?>
+            <p>
+                <?php print $daiktas['info']; ?>
+            </p>
+        <?php endforeach; ?>
     </body>
 </html>
