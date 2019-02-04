4f14a6639c55152d7ce4a554015e6209905b920f
Mantas <mantas.aaaa@gmail.com>
2019-02-04 20:17:07 +0200

    nd

--- index.php
+++ index.php
@@ -1,65 +1,62 @@
 <?php
-$bbd = [
-    [
-        'text' => 'As',
-        'color' => 'green',
-        'form' => 'apskritimas',
-    ],
-    [
-        'text' => 'b',
-        'color' => 'green',
-        'form' => 'kvadratas',
-    ],
-    [
-        'text' => 'b',
-        'color' => 'orange',
-        'form' => 'kvadratas',
-    ],
-    [
-        'text' => 'd',
-        'color' => 'red',
-        'form' => 'kvadratas',
-    ],
-];
+function bbz($l) {
+    $jokes = [
+        [
+            'text' => 'Nuogas vyras prieš veidrodį: <br>
+                        -Būtu 2cm ilgesnis, būčiau karalius! <br>
+                        Žmona: <br>
+                        -O jei 2cm trumpesnis - karalienė.',
+            'color' => 'gold',
+        ],
+        [
+            'text' => ' Kodėl estai vakarienei valgo pakepintas pupas? <br>
+                        Kad ryte, maudydamiesi, maudytusi vonioje su burbuliukais.',
+            'color' => 'silver',
+        ],
+        [
+            'text' => ' - Neseniai buvau parašęs skelbimą,
+                          kad visi idijotai penktadienį privalo susirinkti miesto aikštėje.
+                          Susirinko visas miestas! <br>
+                        - Kodel? <br>
+                        - Visiem buvo įdomu kas ateis.',
+            'color' => 'brown',
+        ],
+    ];
 
-function color_change($arr, $l) {
-    foreach ($arr as $key => &$param) {
+    $new_arr = [];
+
+    foreach ($jokes as $key => $param) {
         if ($key <= $l) {
-            $param['color'] = 'red';
-        } else {
-            $param['color'] = 'grey';
-        };
-
-        $param['show_text'] = false;
-        $arr[$l]['show_text'] = true;
+            $new_arr[] = $param;
+        }
     };
 
-    return $arr;
+    return $new_arr;
 };
 
-$bbd = color_change($bbd, 1);
-/* var_dump($bbd); */
+$jokes = bbz(2);
+/* var_dump($jokes); */
 ?>
 <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-        <link rel="stylesheet" href="css/bbd2.css">
+        <link rel="stylesheet" href="css/story.css">
     </head>
     <body>
-        <div class='flex-container'>
-            <?php foreach ($bbd as $param): ?>
-                <div class='block <?php print $param['form'].' '.$param['color']; ?>'>
-                    <?php if ($param['show_text']): ?>
-                        <span>
-                            <?php print $param['text']; ?>
-                        </span>
-                    <?php endif; ?>
-                </div>
-            <?php endforeach; ?>
+        <div class='jokes'>
+            <ul>
+                <?php foreach ($jokes as $joke): ?>
+                    <li>
+                        <p class='<?php print $joke['color']; ?>'>
+                            <?php print $joke['text']; ?>
+                        </p>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </body>
 </html>
