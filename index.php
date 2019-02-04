4b8b106b746321c3fccd8ff9e0a410985a0ab571
Mantas <mantas.aaaa@gmail.com>
2019-02-04 10:17:53 +0200

    1uzd

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$bbd = [
+    'color' => [
+        'green', 'orange', 'red'
+    ],
+    'words' => [
+        'as', 'b', 'b', 'd'
+    ],
+    'form' => [
+        'apkskritimas', 'kvadratas'
+    ]
+];
+var_dump($bbd);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
