7a453a110ef12ccb2453ad531599deab7ddd6877
Mantas <mantas.aaaa@gmail.com>
2019-02-06 19:32:02 +0200

    ducho anketa

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-$nr = 0; 
+$class = 'invisible';
+$amzius = '';
+$lygis = '';
 
 if (isset($_POST['btn'])) {
-    $nr = $_POST['btn'] + 1;
-}
+    $class = 'visible';
+};
+
+if (isset($_POST['how_old'])) {
+    $amzius = empty($_POST['how_old']) ? '' : 'Amzius: '. $_POST['how_old']. ' metu';
+};
+
+if (isset($_POST['level'])) {
+    $lygis = 'Lygis: '.$_POST['level'];
+};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,13 +22,40 @@ if (isset($_POST['btn'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <link rel="stylesheet" href="css/form.css">
     </head>
     <body>
         <form method='POST'>
-            <button type='submit' name='btn' value='<?php print $nr; ?>'><?php print $nr; ?></button>
+            <label>
+                <span>Vardas:<span>
+                <input type='text' name='name' required>
+            </label>
+            <br>
+            <label>
+                <span>Pavarde:</span>
+                <input type='text' name='last_name' required>
+            </label>
+            <br>
+            <label>
+                <span>Amzius:</span>
+                <input type='number' name='how_old'>
+            </label>
+            <label>
+                <span>Lygis:</span>
+                <select name='level'>
+                    <option value='Chuilo' selected>Chuilo</option>
+                    <option value='Puse_b*b*o'>Puse b*b*o</option>
+                    <option value='Profesionalas'>Profesionalas</option>
+                </select>
+            </label>
+            <button type='submit' name='btn'>Submit</button>
         </form>
-        <?php for ($i = 0; $i < $nr; $i++): ?>
-            <img src='img/fuckit.jpg'>
-        <?php endfor; ?>
+        <div id='ducho-sertifikatas' class='<?php print $class; ?>'>
+            <h2>Ducho Sertifikatas<h2>
+            <h3>Vardas: <?php print $_POST['name']; ?><h3>
+            <h3>Pavarde: <?php print $_POST['last_name']; ?><h3>
+            <h3><?php print $amzius; ?><h3>
+            <h3><?php print $lygis; ?></h3>
+        </div> 
     </body>
 </html>
