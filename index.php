07da285b6343f9fa5402786147c5f5595336f04f
Mantas <mantas.aaaa@gmail.com>
2019-02-07 11:57:54 +0200

    upload files

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-$class = 'invisible';
-$amzius = '';
-$lygis = '';
+$file = $_FILES['photo'] ?? false;
+$dir = 'img';
 
-if (isset($_POST['btn'])) {
-    $class = 'visible';
+function save_file($file, $dir) {
+    if ($file['error'] == 0) {
+        $target_file_name = time() . '-' . $file['name'];
+        $target_path = $dir . '/' . $target_file_name;
+        if (move_uploaded_file($file['tmp_name'], $target_path)) {
+            return true;
+        }
+       return false; 
+    };   
 };
 
-if (isset($_POST['how_old'])) {
-    $amzius = empty($_POST['how_old']) ? '' : 'Amzius: '. $_POST['how_old']. ' metu';
+if (!empty($file)) {
+    save_file($file, $dir);
 };
 
-if (isset($_POST['level'])) {
-    $lygis = 'Lygis: '.$_POST['level'];
-};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,40 +25,14 @@ if (isset($_POST['level'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
-        <link rel="stylesheet" href="css/form.css">
     </head>
     <body>
-        <form method='POST'>
+        <form enctype='multipart/form-data' method='POST'>
             <label>
-                <span>Vardas:<span>
-                <input type='text' name='name' required>
+                <span>Tavo foto:</span>
+                <input name='photo' type='file'>
+                <input type='submit' value='Upload'>
             </label>
-            <br>
-            <label>
-                <span>Pavarde:</span>
-                <input type='text' name='last_name' required>
-            </label>
-            <br>
-            <label>
-                <span>Amzius:</span>
-                <input type='number' name='how_old'>
-            </label>
-            <label>
-                <span>Lygis:</span>
-                <select name='level'>
-                    <option value='Chuilo' selected>Chuilo</option>
-                    <option value='Puse_b*b*o'>Puse b*b*o</option>
-                    <option value='Profesionalas'>Profesionalas</option>
-                </select>
-            </label>
-            <button type='submit' name='btn'>Submit</button>
         </form>
-        <div id='ducho-sertifikatas' class='<?php print $class; ?>'>
-            <h2>Ducho Sertifikatas<h2>
-            <h3>Vardas: <?php print $_POST['name']; ?><h3>
-            <h3>Pavarde: <?php print $_POST['last_name']; ?><h3>
-            <h3><?php print $amzius; ?><h3>
-            <h3><?php print $lygis; ?></h3>
-        </div> 
     </body>
 </html>
